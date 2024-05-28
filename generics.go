2fb6b5a68d96cc72867a5da5f7b1f3041e90a1c1
Muturi-002 <martinmnjoroge03@gmail.com>
2024-05-28 12:50:17 +0300

    first commit

---
+++ generics.go
@@ -0,0 +1,14 @@
+//courtesy of FreeCodeCamp Go assets in Youtube
+package main
+import "fmt"
+func splitanySlice[t any](slice []t)(){
+	for i,slice:=range slice{
+		fmt.Println(i,"-> ",slice)
+	}
+	fmt.Println("=====")
+}
+func main(){
+	fmt.Print("Start\n")
+	splitanySlice([] int{0,2,3,8,39,47})
+	splitanySlice([] string{"boy","girl","mother","father","bigboy"})
+}
